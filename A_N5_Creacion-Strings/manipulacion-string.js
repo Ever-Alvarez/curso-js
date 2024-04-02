f0714d6ad4de57e2cbb630b919732dcadc08b1c1
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-04-02 17:22:05 -0600

    Manipulacion de strings - Acceder a caracteres

--- A_N5_Creacion-Strings/manipulacion-string.js
+++ A_N5_Creacion-Strings/manipulacion-string.js
@@ -14,3 +14,48 @@ const stringObjeto = new String('Soy un string objeto');
 console.log(typeof stringObjeto);
 
 // Acceder a Caracteres
+
+const saludo = 'Hola. ¿Cómo estás?';
+
+//Imprime el caracter que tenga el indice 2
+console.log(saludo[2]);
+console.log(saludo.charAt(2));
+
+//Saber el indice de un caracter
+console.log(saludo.indexOf('o'));
+
+//Saber el indice de una palabra que existe. Devolvera el indice de donde empieza el primer caracter de la palabra
+//Devolvera -1 si la palabra no existe
+console.log(saludo.indexOf('Cómo'));
+
+//Saber la ultima posicion de un caracter
+console.log(saludo.lastIndexOf('o'));
+
+//Saber que caracteres existen entre "x" y "x" indice
+//El primer parametro es el indice desde donde va a empezar
+//El ultimo  parametro es el indice donde termina+1
+console.log(saludo.slice(3, 5));
+
+//Saber la cantidad de caracteres del string
+console.log(saludo.length);
+
+//Convertir el string en mayusculas
+console.log(saludo.toLocaleUpperCase());
+
+//Convertir el string en minuscula
+console.log(saludo.toLocaleLowerCase());
+
+//Dividir sting
+//Separa el string basandode con parametro en split y lo devuelve en arrays (Funciona casi como explode en php)
+const saludoDividido = saludo.split(' ');
+console.log(saludoDividido);
+
+//String sin espacios
+const saludoConEspacios = ' Hola Mundo ';
+const saludoSinEspacios = saludoConEspacios.trim();
+console.log(saludoSinEspacios);
+
+//Reemplazar palabra o caracteres
+const saludoOriginal = 'Hola Mundo';
+const nuevoSaludo = saludoOriginal.replace('Mundo', '😁');
+console.log(nuevoSaludo);
